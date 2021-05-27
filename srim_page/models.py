9ad9125dbc21bea72b9d28eb09c093635d366898
Jaehong Koo <rnwoghd55@gmail.com>
2021-05-28 08:11:31 +0900

    started app srim_page

---
+++ srim_page/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Stock(models.Model):
+    code = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
+    sector = models.CharField(max_length=100)
+    current_price = models.CharField(max_length=100)
+    srim_price = models.CharField(max_length=100)
+    srim10_price = models.CharField(max_length=100)
+    srim20_price = models.CharField(max_length=100)
+    roe = models.CharField(max_length=20, null=True)
+    gap = models.CharField(max_length=20, null=True)
+    risky = models.CharField(max_length=20)
+    created_at = models.DateTimeField(default=now)
