9bbe19e2623e5ef733da48a0669a8eb4999a4886
Jaehong Koo <rnwoghd55@gmail.com>
2021-06-23 22:25:55 +0900

    changed Debug

--- srim_project/settings.py
+++ srim_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-exz*3+^9%%27@_s9x22(*iz8!k1&s&141kd$s^xr9$w=7d&r=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['3.37.5.6', 'srimfinder.com', 'www.srimfinder.com']
 
